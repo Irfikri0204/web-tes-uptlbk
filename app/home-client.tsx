95d0376316ed81ee9eb0134be3eea7af016e603a
Ir'Fikri Anggara Hasbar <fikrianggara72@gmail.com>
2025-07-20 00:32:18 +0800

    third commit

---
+++ app/home-client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import SplashScreen from "./components/SplashScreen";
+import MainPage from "./components/MainPage";
+
+export default function HomeClient() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+
+  return <MainPage />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
-"use client";
+export const metadata = {
+  title: "Halaman Tes Kepribadian",
+  description: "Halaman untuk memilih tes kepribadian yang kamu inginkan.",
+};
 
-import { useEffect, useState } from "react";
-import SplashScreen from "./components/SplashScreen";
-import MainPage from "./components/MainPage";
+import Home from "./home-client";
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 300);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) {
-    return <SplashScreen />;
-  }
-  return <MainPage />;
+export default function Page() {
+  return <Home />;
 }
